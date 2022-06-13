5c5e0bc08d89d4b212bdc9498c0a7200aa94d151
Administrador <jeremyponce2012@gmail.com>
2022-06-13 08:54:02 -0500

    Agregado menu y mas acosas

---
+++ app/src/main/java/com/flaremy/uvmedia/Start_interactive.kt
@@ -0,0 +1,11 @@
+package com.flaremy.uvmedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Start_interactive : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_interactive)
+    }
+}
