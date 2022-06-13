fc02f3b02b55c4dcf224fe84917e735bf6f6ed02
Administrador <jeremyponce2012@gmail.com>
2022-06-13 08:34:17 -0500

    Agregado el activity_splash_image.xml

---
+++ app/src/main/java/com/flaremy/uvmedia/SplashImage.kt
@@ -0,0 +1,11 @@
+package com.flaremy.uvmedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashImage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_image)
+    }
+}
